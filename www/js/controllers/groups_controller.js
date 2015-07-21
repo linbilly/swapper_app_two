1839f0148de0ace87a9e553f64020ed09dd27f2f
linbilly <billy.lin@gmail.com>
2015-07-20 19:34:51 -0700

    loader

--- www/js/controllers/groups_controller.js
+++ www/js/controllers/groups_controller.js
@@ -1,12 +1,14 @@
 angular.module('starter.controllers')
 
 .controller('GroupsCtrl', function($scope, Api) {
+  $scope.loader = true;
   $scope.fetchGroups = function() {
     Api.getGroups()
   }
 
   $scope.$on('groupsFetched', function() {
     $scope.groups = Api.groups
+    $scope.loader = false;
     $scope.$apply();
   });
 })
