57bc2c16e32d321601c4089409b8d74bb7fbf35e
David Wen <dstwen@gmail.com>
2015-08-22 13:45:27 -0700

    Show shift type times on shift types index tab

--- www/js/controllers/shifts_patterns_controller.js
+++ www/js/controllers/shifts_patterns_controller.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.controller('ShiftsPatternsCtrl', function($scope, $ionicPopover, $ionicNavBarDelegate, Api, ShiftPopover, Notification) {
+.controller('ShiftsPatternsCtrl', function($scope, $ionicPopover, $ionicNavBarDelegate, Api, ShiftPopover, Notification, ShiftType) {
   $ionicNavBarDelegate.showBackButton(false)
   ShiftPopover.renderPopover($scope)
   $scope.loader = true;
@@ -22,4 +22,22 @@ angular.module('starter.controllers')
     $scope.groupsWithShiftTypes = Api.groupsWithShiftTypes
     $scope.loader = false;
   });
+
+  $scope.prettyEndTime = function(shiftType) {
+    var now = new Date()
+    var shiftStart = new Date(now.getFullYear(), now.getMonth(), now.getDay(), shiftType.start_hour, shiftType.start_minute)
+    var shiftEnd = new Date(shiftStart.getTime() + shiftType.duration * 60 * 1000)
+    var hours = shiftEnd.getHours()
+    var minutes = shiftEnd.getMinutes()
+
+    if (shiftEnd.getHours() < 10) {
+      hours = "0" + shiftEnd.getHours().toString()
+    }
+
+    if (shiftEnd.getMinutes() < 10) {
+      minutes = "0" + shiftEnd.getMinutes().toString()
+    }
+
+    return hours + ":" + minutes
+  }
 })
