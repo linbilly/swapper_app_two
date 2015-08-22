79ddffc3d397f324721d1ee1660ea08911bf2e3a
David Wen <dstwen@gmail.com>
2015-08-21 20:04:47 -0700

    Moved numberFixedLen to Time Adjuster service

--- www/js/controllers/shifts_patterns_detail_controller.js
+++ www/js/controllers/shifts_patterns_detail_controller.js
@@ -56,18 +56,3 @@ angular.module('starter.controllers')
     Api.updateShiftPattern($stateParams.patternId, shiftParams)
   }
 })
-
-.filter('numberFixedLen', function () {
-  return function (n, len) {
-    var num = parseInt(n, 10);
-    len = parseInt(len, 10);
-    if (isNaN(num) || isNaN(len)) {
-      return n;
-    }
-    num = ''+num;
-    while (num.length < len) {
-      num = '0'+num;
-    }
-    return num;
-  };
-});
