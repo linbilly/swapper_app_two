76808a8efbeded9f1fb979e18845aa939c5a12c4
David Wen <dstwen@gmail.com>
2015-08-25 16:55:44 -0700

    Add styled (but non-functional) input buttons

--- www/js/controllers/shifts_input_controller.js
+++ www/js/controllers/shifts_input_controller.js
@@ -3,5 +3,5 @@ angular.module('starter.controllers')
 .controller('ShiftsInputCtrl', function($scope, $ionicPopover, $ionicNavBarDelegate, $ionicSlideBoxDelegate, Api, Calendar) {
   $ionicNavBarDelegate.showBackButton(false)
 
-  
+  $scope.shiftTypes = ["Dx", "Nx"]
 })
