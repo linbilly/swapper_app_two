201e4d59866180d9e5f39c0d87ad0c3a04333a7a
David Wen <dstwen@gmail.com>
2015-08-25 13:20:36 -0700

    Refactor to one calendar controlled by CalendarController

--- www/js/controllers/shifts_input_controller.js
+++ www/js/controllers/shifts_input_controller.js
@@ -3,13 +3,5 @@ angular.module('starter.controllers')
 .controller('ShiftsInputCtrl', function($scope, $ionicPopover, $ionicNavBarDelegate, $ionicSlideBoxDelegate, Api, Calendar) {
   $ionicNavBarDelegate.showBackButton(false)
 
-  $scope.calendarObjects = Calendar.setupCalendarObjects()
-
-  $scope.previousSlide = function() {
-    $ionicSlideBoxDelegate.$getByHandle('input').previous()
-  }
-
-  $scope.nextSlide = function() {
-    $ionicSlideBoxDelegate.$getByHandle('input').next()
-  }
+  
 })
--- www/js/controllers/shifts_swap_controller.js
+++ www/js/controllers/shifts_swap_controller.js
@@ -3,13 +3,5 @@ angular.module('starter.controllers')
 .controller('ShiftsSwapCtrl', function($scope, $ionicPopover, $ionicNavBarDelegate, $ionicSlideBoxDelegate, Api, Calendar) {
   $ionicNavBarDelegate.showBackButton(false)
 
-  $scope.calendarObjects = Calendar.setupCalendarObjects()
-
-  $scope.previousSlide = function() {
-    $ionicSlideBoxDelegate.$getByHandle('swap').previous()
-  }
-
-  $scope.nextSlide = function() {
-    $ionicSlideBoxDelegate.$getByHandle('swap').next()
-  }
+  
 })
