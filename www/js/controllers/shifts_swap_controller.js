756edb50d20eef8e60ed62a9f625f5a589bd8198
David Wen <dstwen@gmail.com>
2015-09-01 10:46:05 -0700

    Further organization of calendar controllers

--- www/js/controllers/shifts_swap_controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('starter.controllers')
-
-.controller('ShiftsSwapCtrl', function($scope, $ionicPopover, $ionicNavBarDelegate, $ionicSlideBoxDelegate, Api, Calendar) {
-  $ionicNavBarDelegate.showBackButton(false)
-
-  
-})
