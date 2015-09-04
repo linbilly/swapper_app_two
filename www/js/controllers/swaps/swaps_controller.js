d7f6868aa2428fa564d51d9eb6b10fe366ff18a1
David Wen <dstwen@gmail.com>
2015-09-04 13:35:27 -0700

    Allow top tab state on swaps to keep even when changing major tabs

--- www/js/controllers/swaps/swaps_controller.js
+++ www/js/controllers/swaps/swaps_controller.js
@@ -1,10 +1,16 @@
 angular.module('starter.controllers')
 
-.controller('SwapsCtrl', function($scope, $ionicNavBarDelegate, Api, General, Notification) {
+.controller('SwapsCtrl', function($rootScope, $scope, $ionicNavBarDelegate, Api, General, Notification) {
   $ionicNavBarDelegate.showBackButton(false)
   $scope.loader = true
   if (Notification.message) {
     $scope.message = Notification.message
     Notification.message = null
   }
+
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    if (toState.url == "/swaps/index") {
+      Api.getOwnShiftsWithSwaps()
+    }
+  })
 })
