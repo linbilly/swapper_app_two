797c70b5643b49ec164e2f178ecad683b7eba5c8
David Wen <dstwen@gmail.com>
2015-09-05 17:07:13 -0700

    Able to select multiple cells

--- www/js/controllers/swaps/swaps_new_offer_controller.js
+++ www/js/controllers/swaps/swaps_new_offer_controller.js
@@ -17,4 +17,22 @@ angular.module('starter.controllers')
     $scope.calendarObjects = Calendar.setupCalendarObjects(args.shifts, {})
     $scope.loader = false
   })
+
+  $scope.multipleDatesSelected = function($event) {
+    var ele = null
+    if ($($event.target).hasClass("date-col")) {
+      ele = $($event.target)
+    } else {
+      ele = $($event.target).parents(".date-col")
+    }
+    var selectedDate = ele.attr("data-date")
+    var selectedCells = $(".dates").find("[data-date='" + selectedDate + "']");
+    for (var i = 0; i < selectedCells.length; i++) {
+      if ($(selectedCells[i]).hasClass("active")) {
+        $(selectedCells[i]).removeClass("active")
+      } else {
+        $(selectedCells[i]).addClass("active")
+      }
+    };
+  }
 })
