888529507836c949eb3b9d30ec08726e864002e7
David Wen <dstwen@gmail.com>
2015-09-16 14:43:51 -0700

    Improve look of group creation and go to groups index after

--- www/js/controllers/general/groups/group_create_controller.js
+++ www/js/controllers/general/groups/group_create_controller.js
@@ -1,10 +1,11 @@
 angular.module('starter.controllers')
 
-.controller('GroupCreateCtrl', function($scope, $ionicNavBarDelegate, Api) {
+.controller('GroupCreateCtrl', function($scope, $state, $ionicNavBarDelegate, Api, Notification) {
   $ionicNavBarDelegate.showBackButton(false)
 
   $scope.createGroup = function (groupName){
     Api.createGroup(groupName)
+    Notification.message = groupName + " successfully created."
+    $state.go('tab.groups', {}, {reload: true});
   }
-
 })
--- www/js/controllers/general/groups/groups_controller.js
+++ www/js/controllers/general/groups/groups_controller.js
@@ -1,14 +1,18 @@
 angular.module('starter.controllers')
 
-.controller('GroupsCtrl', function($scope, $ionicNavBarDelegate, Api) {
+.controller('GroupsCtrl', function($scope, $ionicNavBarDelegate, Api, Notification) {
   $ionicNavBarDelegate.showBackButton(false)
   $scope.loader = true;
-  $scope.fetchGroups = function() {
-    Api.getGroups()
-  };
+
+  Api.getGroups()
 
   $scope.$on('groupsFetched', function() {
     $scope.groups = Api.groups
     $scope.loader = false;
   });
+
+  if (Notification.message) {
+    $scope.message = Notification.message
+    Notification.message = null
+  }
 })
