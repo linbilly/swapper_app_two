d2a1b6b33b0556dc707ad78b4791488907eb8901
David Wen <dstwen@gmail.com>
2015-09-17 10:22:16 -0700

    Improve usability of commenting

--- www/js/controllers/swaps/comments/comments_controller.js
+++ www/js/controllers/swaps/comments/comments_controller.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.controller('CommentsCtrl', function($scope, $stateParams, $ionicNavBarDelegate, Api, General, ShiftType) {
+.controller('CommentsCtrl', function($scope, $stateParams, $ionicNavBarDelegate, $ionicScrollDelegate, Api, General, ShiftType) {
   $ionicNavBarDelegate.showBackButton(false)
   $scope.loader = true
 
@@ -9,7 +9,9 @@ angular.module('starter.controllers')
   $scope.$on('commentsFetched', function(event, args) {
     $scope.shift = args.shift
     $scope.comments = Api.comments
+    $scope.$apply()
     $scope.loader = false
+    $ionicScrollDelegate.scrollBottom([true])
   });
 
   $scope.stringDateToWords = function(startDate) {
@@ -28,16 +30,15 @@ angular.module('starter.controllers')
   }
 
   $scope.sendMessage = function() {
-    var params = {
-      message: $scope.message,
-      shift_id: $scope.shift.id
+    var message = $scope.message.trim()
+    if (message != "") {
+      var params = {
+        message: message,
+        shift_id: $scope.shift.id
+      }
+      Api.createComment(params)
+      Api.getComments($stateParams.shiftId)
+      $scope.message = ""
     }
-    Api.createComment(params)
-  }
-
-  $scope.jsTime = function(rubyTime) {
-    // 2015-09-17T16:25:37.973Z
-    var date = new Date(rubyTime.split(".")[0])
-    return date
   }
 })
