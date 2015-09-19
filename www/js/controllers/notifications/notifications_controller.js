692b77681f05e07e02726b0163cfbb3f7cc347b3
David Wen <dstwen@gmail.com>
2015-09-19 15:56:37 -0700

    Add loader to notifications

--- www/js/controllers/notifications/notifications_controller.js
+++ www/js/controllers/notifications/notifications_controller.js
@@ -2,8 +2,11 @@ angular.module('starter.controllers')
 
 .controller('NotificationsCtrl', function($scope, Api) {
   Api.getNotifications()
+  $scope.loader = true
 
   $scope.$on('notificationsFetched', function(event, args) {
     $scope.notifications = Api.notifications
+    $scope.loader = false
+    $scope.$apply()
   })
 })
