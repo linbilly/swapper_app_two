7e01752f9fbb6a08e39236c09b8d587c5df8d414
David Wen <dstwen@gmail.com>
2015-09-22 21:42:48 -0700

    Validations before creating group

--- www/js/controllers/general/groups/group_create_controller.js
+++ www/js/controllers/general/groups/group_create_controller.js
@@ -2,6 +2,7 @@ angular.module('starter.controllers')
 
 .controller('GroupCreateCtrl', function($scope, $state, $ionicNavBarDelegate, Api, Notification, Locations, Group) {
   $ionicNavBarDelegate.showBackButton(false)
+  clearErrors()
 
   displayGroupTitle()
   displayCountry()
@@ -44,10 +45,13 @@ angular.module('starter.controllers')
   }
 
   $scope.createGroup = function (groupName){
-    Api.createGroup(groupName)
-    Notification.message = groupName + " successfully created."
-    clearAllSavedValues()
-    $state.go('tab.groups', {}, {reload: true});
+    clearErrors()
+    if (noErrors(groupName)) {
+      Api.createGroup(groupName)
+      Notification.message = groupName + " successfully created."
+      clearAllSavedValues()
+      $state.go('tab.groups', {}, {reload: true});
+    }
   }
 
   $scope.storeGroupTitle = function(groupTitle) {
@@ -62,5 +66,60 @@ angular.module('starter.controllers')
     Locations.selectedProvinceId = null
     Locations.selectedStateName = null
     Locations.selectedStateId = null
+  }
+
+  function noErrors(groupName) {
+    return groupNameFilledIn(groupName) && countrySelected() && provinceSelectedIfRequired() && stateSelectedIfRequired()
+  }
+
+  function clearErrors() {
+    $scope.groupNameError = false
+    $scope.countryError = false
+    $scope.provinceError = false
+    $scope.stateError = false
+  }
+
+  function groupNameFilledIn(groupName) {
+    if (groupName.trim() == "") {
+      $scope.groupNameError = true
+      return false
+    } else {
+      return true
+    }
+  }
+
+  function countrySelected() {
+    if (Locations.selectedCountryName) {
+      return true
+    } else {
+      $scope.countryError = true
+      return false
+    }
+  }
+
+  function provinceSelectedIfRequired() {
+    if (Locations.selectedCountryId == "ca") {
+      if (Locations.selectedProvinceName) {
+        return true
+      } else {
+        $scope.provinceError = true
+        return false
+      }
+    } else {
+      return true
+    }
+  }
+
+  function stateSelectedIfRequired() {
+    if (Locations.selectedCountryId == "us") {
+      if (Locations.selectedStateName) {
+        return true
+      } else {
+        $scope.stateError = true
+        return false
+      }
+    } else {
+      return true
+    }
   }
 })
