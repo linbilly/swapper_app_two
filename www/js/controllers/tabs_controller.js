dac878aab40f64e1a94187da85fe9139627a5087
David Wen <dstwen@gmail.com>
2015-10-02 08:54:42 -0700

    Handle setting badges for Android as well

--- www/js/controllers/tabs_controller.js
+++ www/js/controllers/tabs_controller.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('TabsCtrl', function($rootScope, $scope, $location, $interval, $cordovaPush, $cordovaDialogs, $cordovaMedia, $state, Api, Notification) {
+.controller('TabsCtrl', function($rootScope, $scope, $location, $interval, $cordovaPush, $cordovaDialogs, $cordovaMedia, $state, $cordovaBadge, Api, Notification) {
   if (hasToken() && !Api.initialSignUp) {
     Api.numUnreadNotifications()
     $interval(Api.numUnreadNotifications, 1000 * 60 * 5) // Check for num of unread notifications every 5 minutes
@@ -22,13 +22,15 @@ angular.module('starter.controllers', [])
   }
 
   function updateIconBadge() {
-    if (ionic.Platform.isIOS()) {
-      $cordovaPush.setBadgeNumber(Notification.numUnread).then(function (result) {
-        console.log("Set badge success")
-      }, function (err) {
-        console.log("Set badge error: " + err)
+    $cordovaBadge.hasPermission().then(function(yes) {
+      $cordovaBadge.set(Notification.numUnread).then(function() {
+        console.log("Badge set to: " + Notification.numUnread)
+      }, function(err) {
+        console.log(err)
       });
-    }
+    }, function(no) {
+      console.log(no)
+    });
   }
 
   function hasToken() {
